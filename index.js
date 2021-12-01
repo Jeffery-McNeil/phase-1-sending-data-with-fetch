3e80b1c935a73a3f953712362e90b1b7f9617bae
Jeffery <jeffmc825@gmail.com>
2021-11-30 20:52:10 -0600

    commit tree cd6f993c5aa8e6eaf268867b8ca1652974c43fef

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const configurationObject = {
+        method: "POST",   
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            },
+            body: JSON.stringify({
+                name: userName,
+                email: userEmail
+            }),
+        };
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object){
+        const li = document.createElement('li')
+        li.textContent = object.id
+        document.querySelector('body').append(li)
+    })
+    .catch(function (error){
+        const li = document.createElement('li')
+        li.textContent = error.message
+        document.querySelector('body').append(li)
+    })
+}
